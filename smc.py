4d04b3e4aa91dff19d1e1cf191c6b502d777ef93
Harald Schilly <harald.schilly@gmail.com>
2015-02-28 16:24:20 +0100

    html email, and json.dumps doesnt work to get xhr status text to be OK

--- smc.py
+++ smc.py
@@ -40,15 +40,33 @@ GLOBAL_VALS = {
     "slogan": "Collaborative Computational Mathematics Online"
 }
 
+# double {{ }} for { } because {fields} are replaced via string.format()
 TMPL_EMAIL = u"""\
-Name:       {name}
-Email:      {email}
-Telephone:  {tel}
-Subject:    {subject}
-Timestamp:  {timestamp}
-IP:         {ip}
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<style>
+body {{ font-family:‘Lucida Console’, Monaco, monospace; }}
+table {{border-collapse: collapse; }}
+table td {{ padding: 10px; }}
+</style>
+</head>
+<body style="font-family:‘Lucida Console’, Monaco, monospace;">
+<table border="1">
+<tr><td>Name:</td>      <td>{name}</td></tr>
+<tr><td>Email:</td>     <td>{email}</td></tr>
+<tr><td>Telephone:</td> <td>{tel}</td></tr>
+<tr><td>Subject:</td>   <td>{subject}</td></tr>
+<tr><td>Timestamp:</td> <td>{timestamp}</td></tr>
+<tr><td>IP:</td>        <td>{ip}</td></tr>
+<tr><td colspan="2">
 {message}
+</td></tr>
+</table>
+</body>
+</html>
 """
 
 j2env = jinja2.Environment(
@@ -105,11 +123,13 @@ class ContactForm(webapp2.RequestHandler):
         msg.sender="SMC Website <website@sage-math-inc.appspotmail.com>"
         msg.subject="[SMC.com] %s" % fields.get("subject")
         msg.to = "SageMath Inc. <contact+website@sagemath.com>"
-        msg.body = TMPL_EMAIL.format(timestamp=timestamp, ip = ip, **fields)
+        #msg.to = "hsy@sagemath.com"
+        msg.html = TMPL_EMAIL.format(timestamp=timestamp, ip = ip, **fields)
         msg.send()
 
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.write(json.dumps({ 'success': True }))
+        #self.response.write(json.dumps({ 'success': True }))
+        self.response.write("{ 'success': 'true' }")
 
 
 class Stats(webapp2.RequestHandler):
